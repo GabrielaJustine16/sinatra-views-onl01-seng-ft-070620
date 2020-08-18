61acf79d5924f579d52a577f84779e69b1a0965a
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-18 23:52:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,23 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-	  "<h1>Hello World</h1>"
-		
+		erb :index
 	end
+
+	get '/info' do
+		erb :info
+	end 
 	
-	get '/' do
-      erb :index
-    end
-    
-    get "/info" do
-      "Testing the info page"
-    end
-    
-    get '/' do
-      erb :index
-    end
- 
-    get "/info" do
-      erb :info
-    end
 end
